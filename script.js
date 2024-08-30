863e1aac5b55abf50ba1a346f466f138f4330b28
Harmonyss <lucasomacedo@gmail.com>
2024-08-29 21:06:36 -0300

    Agr vai

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
-document.querySelector('.form').addEventListener('submit', function (event) {
-
-    event.preventDefault();
-
-
-    const name1 = document.getElementById('nome1').value;
-    const name2 = document.getElementById('nome2').value;
-    const name3 = document.getElementById('nome3').value;
-    const name4 = document.getElementById('nome4').value;
-    const message = document.getElementById('mensagem').value;
-
+function enviarFormulario() {
+    const nome1 = document.getElementById('nome1').value;
+    const nome2 = document.getElementById('nome2').value;
+    const nome3 = document.getElementById('nome3').value;
+    const nome4 = document.getElementById('nome4').value;
+    const mensagem = document.getElementById('mensagem').value;
 
     const data = {
-
-        names: [name1, name2, name3, name4],
-        message: message
-
+        names: [nome1, nome2, nome3, nome4],
+        message: mensagem
     };
 
-    });
-
     fetch('https://fsdt-contact.onrender.com/contact', {
-
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-
     })
-        .then(response => {
-
-            if (!response.ok) {
-                throw new Error('Erro na requisição');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Success:', data);
-            alert('Mensagem enviada com sucesso!');
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            alert('Ocorreu um erro ao enviar a mensagem.');
-        });
-
+    .then(response => response.json())
+    .then(data => {
+        alert('Mensagem enviada com sucesso!');
+        console.log('Sucesso:', data);
+    })
+    .catch((error) => {
+        alert('Erro ao enviar a mensagem!');
+        console.error('Erro:', error);
+    });
+}
